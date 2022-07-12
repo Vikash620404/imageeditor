b178441783ac7a95fbb7b0b1605851320c4b79bc
vikash kumar <vikashkumar620404800@gmail.com>
2022-07-13 03:09:03 +0530

    complete project Code

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Image Editor",
       theme: ThemeData.light(),
-      home: const HomeScreen(
-
-      ),
+      home: const HomeScreen(),
       
      
     );
